af56e92d0d2bb4dc1cb2d83fe077f5f30175e470
Matvey Pichaikin <vpicajkin@gmail.com>
2023-11-12 23:34:08 +0500

    Улучшение ввода
    Если вместо цифры введён какой-то другой символ, программа требует повторного ввода.

--- file.cpp
+++ file.cpp
@@ -4,12 +4,35 @@ using namespace std;
 
 int main()
 {
+    int x = 0;
+
     double first_number;
     double second_number;
     char math_operation;
 
-    cout << "Enter two number: ";
-    cin >> first_number >> second_number;
+    cout << "Enter first number: ";
+    while (!(cin >> first_number))
+    {
+        cin.clear();
+        if (x == 0)
+        {
+            cout << "Enter a number!\n";
+            x = 1;
+        }
+        cin.get();
+    }
+
+    cout << "Enter second number: ";
+    while (!(cin >> second_number))
+    {
+        cin.clear();
+        if (x == 0)
+        {
+            cout << "Enter a number!\n";
+            x = 1;
+        }
+        cin.get();
+    }
 
     cout << "Enter a mathematical operation ('+', '-', '*', '/'): ";
     cin >> math_operation;
