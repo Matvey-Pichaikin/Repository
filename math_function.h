f2177279aad6e8dd4c78510c7412cf1eadf5ab7d
Matvey Pichaikin <vpicajkin@gmail.com>
2023-11-12 23:16:31 +0500

    Окончание разработки math_function

--- math_function.h
+++ math_function.h
@@ -1,4 +1,18 @@
+#include <iostream>
+
 double math_function(double first_number, double second_number, char math_operation)
 {
-    
-}
+    if(math_operation == '+')
+        return first_number + second_number;
+    if(math_operation == '-')
+        return first_number - second_number;
+    if(math_operation == '*')
+        return first_number * second_number;
+    if(math_operation == '/')
+        return first_number / second_number;
+    else
+    {
+        std::cout << "Invalid operation entered!\n";
+        return 0;
+    }    
+};
