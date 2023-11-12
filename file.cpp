cb89bb19b58695de5d16f35c5f7294e058bf66bc
Matvey Pichaikin <vpicajkin@gmail.com>
2023-11-12 22:59:20 +0500

    Ввод двух чисел и математической операции.

--- file.cpp
+++ file.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
+    double first_number;
+    double second_number;
+    char math_operation;
+
+    cout << "Enter two number: ";
+    cin >> first_number >> second_number;
+
+    cout << "Enter a mathematical operation ('+', '-', '*', '/'): ";
+    cin >> math_operation;
     
     return 0;
 }
